6074c3135333964a443f68af4afdd6f7deeb35d8
Nikitos <nekit4school@gmail.com>
2023-02-16 14:51:08 +0100

    New file with JS

---
+++ script.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    navigation: {
+        nextEl: '.body-main-block__arrow.swiper-button-next',
+        prevEl: '.body-main-block__arrow.swiper-button-prev',
+    },
+});
